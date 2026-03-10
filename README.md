b28570f92496afdb37d0b13ad9ea532bd5a2f920
pkpacts <pkpacts@gmail.com>
2026-03-10 12:52:12 +0000

    Initial commit: repo structure + moved Terraform

---
+++ README.md
@@ -0,0 +1,2 @@
+# GCP VM + Node.js + Terraform + GitHub Actions
+This repo provisions a GCE VM with Terraform and deploys a Node.js app via GitHub Actions.
---
+++ infra/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "test-project-489406"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_instance" "test_vm" {
+  name         = "test-vm"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
