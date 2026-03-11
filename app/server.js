e2a5139359ad0957f824c3af022dd7b106f02f5c
pkpacts <pkpacts@gmail.com>
2026-03-11 14:05:56 +0000

    Add simple Node.js app

---
+++ app/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Hello Pawan! Deployed from GitHub Actions to GCE 🚀");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
